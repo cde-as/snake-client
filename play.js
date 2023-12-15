601799e63f2980521f287817b837f9c566efda40
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-15 08:34:38 -0800

    created input.js file and exported functions

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 //const net = require("net");
-const connect = require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // your code here
-  if (key === '\u0003') { // letters, numbers, symbols but not arrow keys
-    process.exit(); // Ctrl+C to exit
-  }
-  console.log(`You pressed the button: ${key}`);
-
-};
-
+console.log("Connecting ...");
 connect();
-setupInput();
 
+setupInput();
