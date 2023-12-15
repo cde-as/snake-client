fdc73c34504e4e633fc5bf1c9384dffc3fc78323
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-14 19:51:22 -0800

    created a client file

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost', // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log();
+  });
+
+  conn.on("data", (data) => {
+    //
+    console.log(data);
+  });
+  
+  return conn;
+};
+
+module.exports = connect;
