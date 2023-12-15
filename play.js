7c914fa83bd7d1e2be034fd123ba6605bf874198
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-15 08:19:43 -0800

    setup handleUserInput and setupInput

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 //const net = require("net");
 const connect = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') { // letters, numbers, symbols but not arrow keys
+    process.exit(); // Ctrl+C to exit
+  }
+  console.log(`You pressed the button: ${key}`);
+
+};
 
 connect();
+setupInput();
+
