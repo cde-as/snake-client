11b3d62c5ec294046e076dd64106d7526710c0e0
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-16 17:00:55 -0800

    process.exit() not functioning properly, found process.on(SIGINT) to try and solve this

--- input.js
+++ input.js
@@ -9,9 +9,21 @@ const setupInput = function() {
 
 const handleUserInput = function(key) {
   // your code here
-  if (key === '\u0003') { // letters, numbers, symbols but not arrow keys
-    process.exit(); // Ctrl+C to exit
+  // When client presses Ctrl+C (SIGINT) to exit node
+  process.on("SIGINT", () => {
+    console.log("bye bye");
+    process.exit();
+  });
+
+  if (key === 'w') { // letters, numbers, symbols but not arrow keys
+    console.log("Move: up");
   }
+
+  if (key && key.name === "c" && key.ctrl) {
+    console.log("bye bye");
+    process.exit();
+  }
+
   console.log(`You pressed the button: ${key}`);
 
 };
